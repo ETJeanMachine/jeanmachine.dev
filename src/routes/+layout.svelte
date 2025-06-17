7b7f4ca7110be68b7bc5e151a6334db36c5d878a
Eric Hamilton <ericjean873@gmail.com>
2025-06-17 16:59:20 -0700

    shrunk tab size to two :p

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,46 @@
 <script lang="ts">
-    let { children } = $props();
-    import "@material/web/button/filled-button.js";
-    import "@material/web/button/outlined-button.js";
+  let { children } = $props();
+  import "@material/web/button/filled-button.js";
+  import "@material/web/button/outlined-button.js";
 </script>
 
 <div class="layout">
-    <h1 class="header">Eric Hamilton</h1>
-    <nav class="global-nav">
-        <md-filled-button href="/">Home</md-filled-button>
-        <md-filled-button href="/blog">Blog</md-filled-button>
-        <md-filled-button href="/projects">Projects</md-filled-button>
-    </nav>
-    {@render children()}
+  <h1 class="header">Eric Hamilton</h1>
+  <nav class="global-nav">
+    <md-filled-button href="/">Home</md-filled-button>
+    <md-filled-button href="/blog">Blog</md-filled-button>
+    <md-filled-button href="/projects">Projects</md-filled-button>
+  </nav>
+  {@render children()}
 </div>
 
 <style>
-    :root {
-        --md-sys-color-primary: slategray;
-        --md-sys-color-secondary: lightgray;
-    }
+  :root {
+    --md-sys-color-primary: slategray;
+    --md-sys-color-secondary: lightgray;
+  }
 
-    :global(html, body) {
-        margin: 0;
-        padding: 0;
-        background-color: dimgray;
-        font-family: "Noto Sans", sans-serif;
-    }
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+    background-color: dimgray;
+    font-family: "Noto Sans", sans-serif;
+  }
 
-    .header {
-        text-align: center;
-    }
+  .header {
+    text-align: center;
+  }
 
-    .layout {
-        min-height: 100vh;
-        color: whitesmoke;
-        padding: 20px;
-        box-sizing: border-box;
-    }
+  .layout {
+    min-height: 100vh;
+    color: whitesmoke;
+    padding: 20px;
+    box-sizing: border-box;
+  }
 
-    .global-nav {
-        display: flex;
-        justify-content: center;
-        gap: 1rem;
-    }
+  .global-nav {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  async function get_profile() {
+    const response = await fetch("/api/profile");
+    const data = await response.json();
+    return data;
+  }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
 </p>
