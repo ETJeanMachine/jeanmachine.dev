9707aaa3fdbc71392ca712cf13f8341b3a5b7253
Eric Hamilton <ericjean873@gmail.com>
2025-10-05 17:11:18 -0700

    Improve error page layout and add hover effects

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,13 +5,13 @@
 <div class="container">
   <div class="card">
     <div class="card-content">
-      <div class=""></div>
-      <h1>{$page.status}</h1>
-      <p>{$page.error?.message ?? 'An error occurred'}</p>
+      <h1>{$page.status} - {$page.error?.message ?? 'An error occurred'}</h1>
       <div class="error-gif">
-        <img src="/error.gif" alt="Um... Eto... Bweh!" />
+        <a href="/"
+          >Go home
+          <img src="/error.gif" alt="Um... Eto... Bweh!" />
+        </a>
       </div>
-      <a href="/">Go home</a>
     </div>
   </div>
 </div>
@@ -25,14 +25,32 @@
   }
 
   .card-content {
-    justify-content: center;
-    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
   .error-gif {
     background-color: var(--fg);
-    aspect-ratio: 4 / 1;
     border-radius: 8px;
     border: 5px solid var(--dark3);
+    transition: background-color 0.3s ease;
+  }
+
+  .error-gif:hover {
+    background-color: var(--purple);
+  }
+
+  .error-gif > a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: var(--magenta2);
+    font-style: italic;
+    transition: color 0.3s ease;
+  }
+
+  .error-gif > a:hover {
+    color: var(--magenta);
   }
 </style>
