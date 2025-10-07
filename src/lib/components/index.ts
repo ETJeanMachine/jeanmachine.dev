545e70c611f84c3aa7b18e16a6dc8c6bba70fa43
Eric Hamilton <ericjean873@gmail.com>
2025-10-07 00:12:38 -0700

    Update index.ts

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Profile } from './Profile.svelte';
 export { default as Pinned } from './Pinned.svelte';
+export { default as BlueskyPost } from './BlueskyPost.svelte';
 export { default as Github } from './Github.svelte';
 export { default as About } from './About.svelte';
