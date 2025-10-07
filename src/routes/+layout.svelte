e83baed24cc983e3ec3330b3f0bbc47236faa459
Eric Hamilton <ericjean873@gmail.com>
2025-10-07 16:54:05 -0700

    attempt double scroll fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -189,7 +189,13 @@
 
     main {
       overflow: hidden;
-      position: relative;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      min-height: 100dvh;
+      max-height: 100dvh;
     }
 
     main > div {
