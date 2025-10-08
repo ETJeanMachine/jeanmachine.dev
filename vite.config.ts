851c80b92ce19fd047d1ca333368506c5cacddd4
Eric Hamilton <ericjean873@gmail.com>
2025-10-08 13:41:59 -0700

    vite format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import devtoolsJson from "vite-plugin-devtools-json";
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [sveltekit(), devtoolsJson()],
-});
+export default defineConfig({ plugins: [sveltekit(), devtoolsJson()] });
