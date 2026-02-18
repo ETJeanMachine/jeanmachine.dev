39ccc01bde9865819447651db451397bbaf47151
Eric Hamilton <ericjean873@gmail.com>
2026-02-18 10:43:31 -0800

    style changes to the post

--- src/lib/components/BlueskyPost.svelte
+++ src/lib/components/BlueskyPost.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import { renderPost, type PostData } from 'bluesky-post-embed/core';
-  import 'bluesky-post-embed/style.css';
-  import 'bluesky-post-embed/themes/dark.css';
 
   interface Props {
     data: PostData;
@@ -15,3 +13,18 @@
 <bluesky-post src={data.thread?.post.uri}>
   {@html render}
 </bluesky-post>
+
+<style>
+  @import 'bluesky-post-embed/style.css';
+  @import 'bluesky-post-embed/themes/dark.css';
+
+  :global(.bluesky-embed) {
+    --font-family: 'CaskaydiaCove Nerd Font', sans-serif;
+    --text-primary: var(--primary);
+    --text-link: var(--accent-background);
+    --background-primary: rgb(from var(--page-background) r g b);
+    --background-secondary: rgb(from var(--page-background) r g b / 0.6);
+    --button: var(--accent-background);
+    --button-hover: var(--accent-text);
+  }
+</style>
