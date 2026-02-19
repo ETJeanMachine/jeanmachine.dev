42b1d768a10e4783eb8647226fee0823ee9ff9df
Eric Hamilton <ericjean873@gmail.com>
2026-02-19 10:02:07 -0800

    obfuscated public signal link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,8 +5,7 @@ export const BSKY_HANDLE = 'jeanmachine.dev';
 export const LEAFLET_PUB_RKEY = '3lwddhdcemk2l';
 
 // Social URLs
-export const SIGNAL_URL =
-  'https://signal.me/#eu/8HhSK938QwWPCJ-HMRx9QOG7eFdY2c1kBKW1kcE_K7Xj62YlFJtM1LGY3ZOTnWxW';
+export const SIGNAL_URL = 'https://signal.jean.moe';
 export const EMAIL = 'etj2206@rit.edu';
 export const LINKEDIN_URL = 'https://linkedin.com/in/etjhamilton';
 export const GITHUB_USERNAME = 'ETJeanMachine';
