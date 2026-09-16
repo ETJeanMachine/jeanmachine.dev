d857f11b02350f1c21fba1560b03491180479af5
Eliza Hamilton <ericjean873@gmail.com>
2026-09-16 11:29:38 -0700

    add lucide icon to about for consistency

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,4 +1,10 @@
-<h2>About Me</h2>
+<script>
+  import { Info } from "@lucide/svelte";
+
+</script>
+<h2>
+  <Info size={'1.25rem'} strokeWidth={2.5} />About Me
+</h2>
 <p>
   My name's Eliza, but I also go by Jean! I'm a software engineer from the
   Pacific Northwest with a degree in Computer Science. I love building all sorts
@@ -11,3 +17,12 @@
   in my community at all times - because I believe that staying involved is what
   enables me to make a better impact on the world.
 </p>
+
+<style>
+  h2 {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    align-items: center;
+  }
+</style>
